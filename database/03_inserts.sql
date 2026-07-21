b507eec88cb8277b85f4723203bd872975b73d3f
RodrigoCoradini-Ofic <RodrigoCoradini.Ofic@gmail.com>
2026-07-21 20:39:07 -0300

    Inserindo clientes

---
+++ database/03_inserts.sql
@@ -0,0 +1,9 @@
+-- ==========================================
+-- CLIENTES
+-- ==========================================
+INSERT INTO clientes (nome, email, telefone, cpf, data_cadastro)
+VALUES
+('Rodrigo Coradini', 'rodrigo@gmail.com', '47999999999', '12345678901', '2026-07-20'),
+('Rafael Souza', 'rafael@gmail.com', '47988888888', '12345678902', '2026-07-21'),
+('Maria Eduarda', 'maria@gmail.com', '479777777777', '12345678903', '2026-07-22'),
+('Carlos dos Santos', 'carlos@gmail.com', '479666666666', '12345678904', '2026-07-23');
